ac7a00a7edd1381b6fce1ff1e4f02aa1283a9197
enzowakida <enzo.wakida@gmail.com>
2026-06-10 15:50:48 -0300

    versão corrigida v3

--- src/tratamentoExececao/Exercicio07.java
+++ src/tratamentoExececao/Exercicio07.java
@@ -1,28 +1,27 @@
 package tratamentoExececao;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercicio07 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int fat = 1, num;
+        int fat = 1, num = 0;
 
         try {
             System.out.println("Informe o Fatorial: ");
             num = ler.nextInt();
-
-            if (num < 0) {
-                System.out.println("Não existe fatorial de número negativo");
-                System.exit(0);
-            }
-            for (int i = 1; i <= num; i++) {
-                fat *= i;
-            }
-            System.out.println("O fatorial de " + num + " é: " + fat);
-        } catch (InputMismatchException erro) {
-            System.out.println("Digite um valor que possa ser fatorado");
+        } catch (ArithmeticException erro) {
+            System.out.println("O valor digitado deve ser um número inteiro");
         }
+
+        if (num < 0) {
+            System.out.println("Não existe fatorial de número negativo");
+            System.exit(0);
+        }
+        for (int i = 1; i <= num; i++) {
+            fat *= i;
+        }
+        System.out.println("O fatorial de " + num + " é: " + fat);
         ler.close();
     }
 }
