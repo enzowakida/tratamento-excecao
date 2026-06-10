aa415997da1ba297050d881cef6ec7a00bad8f43
enzowakida <enzo.wakida@gmail.com>
2026-06-10 15:44:30 -0300

    versão corrigida v3

--- src/tratamentoExececao/Exercicio05.java
+++ src/tratamentoExececao/Exercicio05.java
@@ -9,32 +9,32 @@ public class Exercicio05 {
         String resp;
         String continua = "S";
 
-        while(continua.equals("s") || continua.equals("S")) {
-                System.out.println(" ##### Informe o Gabarito ##### ");
-                qtdeAcerto = 0;
+        while (continua.equals("s") || continua.equals("S")) {
+            System.out.println(" ##### Informe o Gabarito ##### ");
+            qtdeAcerto = 0;
 
-                System.out.println("Informe a questão I: ");
-                resp = ler.next();
-                if (resp.equals("A")) {
-                    qtdeAcerto++;
-                }
+            System.out.println("Informe a questão I: ");
+            resp = ler.next();
+            if (resp.equals("A")) {
+                qtdeAcerto++;
+            }
 
-                System.out.println("Informe a questão II: ");
-                resp = ler.next();
-                if (resp.equals("B")) {
-                    qtdeAcerto++;
-                }
+            System.out.println("Informe a questão II: ");
+            resp = ler.next();
+            if (resp.equals("B")) {
+                qtdeAcerto++;
+            }
 
-                System.out.println("Informe a questão III: ");
-                resp = ler.next();
-                if (resp.equals("C")) {
-                    qtdeAcerto++;
-                }
-                somaAcertos = somaAcertos + qtdeAcerto;
-                totalAlunos += 1;
+            System.out.println("Informe a questão III: ");
+            resp = ler.next();
+            if (resp.equals("C")) {
+                qtdeAcerto++;
+            }
+            somaAcertos = somaAcertos + qtdeAcerto;
+            totalAlunos += 1;
 
-                System.out.println("Deseja Continuar? S ou N ");
-                continua = ler.next();
+            System.out.println("Deseja Continuar? S ou N ");
+            continua = ler.next();
 
         }
         System.out.println("Total de acertos: " + somaAcertos);
