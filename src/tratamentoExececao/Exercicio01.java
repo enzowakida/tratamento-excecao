c7d5a5eb3d5e99e9e83843be74b84bafb93a5471
enzowakida <enzo.wakida@gmail.com>
2026-06-10 15:29:51 -0300

    versão corrigida v3

--- src/tratamentoExececao/Exercicio01.java
+++ src/tratamentoExececao/Exercicio01.java
@@ -1,11 +1,13 @@
 package tratamentoExececao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercicio01 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int a, b;
+        int a;
+        int b;
 
         try {
             System.out.println("Informe o primeiro valor: ");
@@ -14,14 +16,21 @@ public class Exercicio01 {
             System.out.println("Informe o segundo valor.: ");
             b = ler.nextInt();
 
+
             System.out.println("Resultados");
             System.out.println("Soma: " + (a + b));
             System.out.println("Subtração: " + (a - b));
             System.out.println("Multiplicação: " + (a * b));
-            System.out.println("Divisão inteira: " + (a % b));
-            System.out.println("Divisão exata: " + ((double) a / b));
-        } catch (RuntimeException erro) {
-            System.out.println("VALOR INVÁLIDO!");
+
+
+            try {
+                System.out.println("Divisão inteira: " + (a % b));
+                System.out.println("Divisão exata: " + ((double) a / b));
+            } catch (ArithmeticException erro) {
+                System.out.println("Não é possível dividir por zero");
+            }
+        } catch (InputMismatchException erro) {
+            System.out.println("Digite apenas números");
         }
         ler.close();
     }
