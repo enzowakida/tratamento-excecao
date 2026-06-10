b0471b2acce9e7eb74c8b757f12ecb0eaed280e2
enzowakida <enzo.wakida@gmail.com>
2026-06-10 15:45:19 -0300

    versão corrigida v3

--- src/tratamentoExececao/Exercicio04.java
+++ src/tratamentoExececao/Exercicio04.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercicio04 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        double v1 = 0, v2 = 0, v3 = 0;
+        double v1, v2, v3;
 
         try {
             System.out.println("Informe o valor 1: ");
